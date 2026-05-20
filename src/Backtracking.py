4ef38c8e0e853837e923e9750166b1b52a07a6cb
xndrxid <andresagudeloram@gmail.com>
2026-05-20 16:11:05 -0500

    obtener_tareas_disponibles, las agregamos a una lista que retornaremos al final. sera la encargada de verificar de las tareas estan disponibles en conjunto de dependencias_cumplidas verificando que estas cumplan los requisitos

--- src/Backtracking.py
+++ src/Backtracking.py
@@ -2,6 +2,8 @@
 # SOLUCIÓN BACKTRACKING
 # -----------------------------------------
 
+
+#dependencias_cumplidas verifica si una tarea puede ser ejecutada, es decir, si todas sus dependencias han sido cumplidas.
 def dependencias_cumplidas(tarea, ejecutadas):
 
     dependencias = tarea[3]
@@ -11,3 +13,20 @@ def dependencias_cumplidas(tarea, ejecutadas):
             return False
 
     return True
+
+#obtener_tareas_disponibles, las agregamos a una lista que retornaremos al final 
+def obtener_tareas_disponibles(tareas, ejecutadas):
+
+    disponibles = []
+
+    for tarea in tareas:
+
+        nombre = tarea[0]
+
+        if nombre in ejecutadas:
+            continue
+
+        if dependencias_cumplidas(tarea, ejecutadas):
+            disponibles.append(tarea)
+
+    return disponibles
