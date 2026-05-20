87bf17d1f9520ec16939b3d8f7c5f86e054ccf85
xndrxid <andresagudeloram@gmail.com>
2026-05-20 16:14:20 -0500

    backtracking es la función principal que implementa el algoritmo de backtracking para encontrar la mejor asignación de tareas a servidores. utilizaremos set() para guardar las tareas y no repetirlas a la hora de buscar. tambien utilizaremos un diccionario para saber cual es nuestro mejor tiempo, esto significa un mejor tiempo. aqui vemos una diferencia con el algoritmo greedy, ya que con greedy nos da 26 en tiempo total y con backtraking nos da un tiempo total de 25

--- src/Backtracking.py
+++ src/Backtracking.py
@@ -30,3 +30,86 @@ def obtener_tareas_disponibles(tareas, ejecutadas):
             disponibles.append(tarea)
 
     return disponibles
+
+#backtracking es la función principal que implementa el algoritmo de backtracking para encontrar la mejor asignación de tareas a servidores.
+def backtracking(tareas,num_servidores,cargas=None,ejecutadas=None,asignacion=None,mejor=None):
+
+    # Inicialización
+    if cargas is None:
+        cargas = [0] * num_servidores
+
+    if ejecutadas is None:
+        ejecutadas = set()
+
+    if asignacion is None:
+        asignacion = []
+
+    if mejor is None:
+        mejor = {"tiempo": float('inf'), "asignacion": None}
+
+    # Caso base
+    if len(ejecutadas) == len(tareas):
+
+        tiempo_total = max(cargas)
+
+        if tiempo_total < mejor["tiempo"]:
+
+            mejor["tiempo"] = tiempo_total
+            mejor["asignacion"] = asignacion.copy()
+
+        return mejor
+
+    # Obtener tareas disponibles
+    disponibles = obtener_tareas_disponibles(tareas,ejecutadas)
+
+    # Ordenar por prioridad
+    disponibles.sort(key=lambda x: x[2])
+
+    # Explorar soluciones
+    for tarea in disponibles:
+
+        nombre = tarea[0]
+        tiempo = tarea[1]
+        prioridad = tarea[2]
+
+        for servidor in range(num_servidores):
+
+            # HACER CAMBIOS
+            cargas[servidor] += tiempo
+
+            # PODA
+            if max(cargas) >= mejor["tiempo"]:
+
+                cargas[servidor] -= tiempo
+                continue
+
+            ejecutadas.add(nombre)
+
+            asignacion.append((nombre, tiempo ,prioridad, servidor))
+
+            # LLAMADA RECURSIVA
+            backtracking(tareas,num_servidores,cargas,ejecutadas,asignacion,mejor)
+
+            # DESHACER CAMBIOS
+            cargas[servidor] -= tiempo
+            ejecutadas.remove(nombre)
+            asignacion.pop()
+
+    return mejor
+
+tareas_grande = [
+    ("T1", 3, 1, []),
+    ("T2", 5, 2, ["T1"]),
+    ("T3", 2, 1, ["T1"]),
+    ("T4", 4, 3, ["T2"]),
+    ("T5", 6, 2, ["T2", "T3"]),
+    ("T6", 3, 1, ["T3"]),
+    ("T7", 7, 2, ["T4", "T5"]),
+    ("T8", 2, 3, ["T6"]),
+    ("T9", 5, 1, ["T6"]),
+    ("T10", 4, 2, ["T7", "T8"]),
+    ("T11", 6, 3, ["T9"]),
+    ("T12", 3, 2, ["T10", "T11"])
+]
+
+print(backtracking(tareas_grande,2))
