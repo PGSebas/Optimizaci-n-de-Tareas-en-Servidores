dbc596dd2c336ed85d2ed426ba1ae86149937a02
Juan Sebastian Pinilla Giraldo <pinillagiraldoj@gmail.com>
2026-05-21 23:32:41 -0500

    funcion dependencias_cumplidas

---
+++ src/fuerza_bruta.py
@@ -0,0 +1,17 @@
+from itertools import product
+
+# -----------------------------------------
+# Verifica dependencias
+# -----------------------------------------
+
+def dependencias_cumplidas(tarea, ejecutadas):
+
+    dependencias = tarea[3]
+
+    for dep in dependencias:
+        if dep not in ejecutadas:
+            return False
+
+    return True
+
+
